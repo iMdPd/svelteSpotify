2f4afe75550d8eaf12581351422f391e45573f1a
iMdPd <politowiczmar@gmail.com>
2024-04-07 20:04:17 +0200

    Creating & Styling the Desktop Navigation Menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
+	import { Navigation } from '$components';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
@@ -9,6 +10,11 @@
 </script>
 
 <div id="main">
+	{#if user}
+		<div id="sidebar">
+			<Navigation desktop={true} />
+		</div>
+	{/if}
 	<div id="content">
 		<main id="main-content">
 			<slot />
@@ -18,7 +24,10 @@
 
 <style lang="scss">
 	#main {
+		display: flex;
 		#content {
+			flex: 1;
+
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up(md) {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,19 @@
 
 <input />
 <select>
-  <option>Option</option>
+	<option>Option</option>
 </select>
 <button>Button</button>
 
-
 <style lang="scss">
-
-  h1{
-    color: red;
-    font-size: functions.toRem(36);
-    @include breakpoint.up('md'){
-      color: green;
-    }
-    @include breakpoint.up('lg'){
-      color: blue;
-    }
-  }
+	h1 {
+		color: red;
+		font-size: functions.toRem(36);
+		@include breakpoint.up('md') {
+			color: green;
+		}
+		@include breakpoint.up('lg') {
+			color: blue;
+		}
+	}
 </style>
